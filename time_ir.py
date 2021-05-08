accec8181f7478e0443e09cc6da7e0463733e8a7
amiwrpremium <amiwrpremium@gmail.com>
2021-05-08 16:38:41 +0430

    Functions Optimized

--- time_ir.py
+++ time_ir.py
@@ -7,64 +7,32 @@ class Time:
     r = requests.get(url)
     soup = bs(r.content, features="lxml")
 
-    def shamsi(self):
-        shamsi_info = self.soup.find('div',
-                                     attrs={'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlShamsi'})
-        shamsi_date = shamsi_info.find('span', attrs={'class': 'show numeral'}).text
-        return shamsi_date
+    shamsi = soup.find('div', attrs={'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlShamsi'}).\
+        find('span', attrs={'class': 'show numeral'}).text
 
-    def miladi(self):
-        miladi_info = self.soup.find('div',
-                                     attrs={
-                                         'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlGregorian'})
-        miladi_date = miladi_info.find('span', attrs={'class': 'show numeral'}).text
+    miladi = soup.find('div', attrs={'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlGregorian'}).\
+        find('span', attrs={'class': 'show numeral'}).text
 
-        return miladi_date
+    ghamari = soup.find('div', attrs={'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlHijri'}).\
+        find('span', attrs={'class': 'show numeral'}).text
 
-    def ghamari(self):
-        ghamari_info = self.soup.find('div',
-                                      attrs={'id': 'ctl00_cphTop_Sampa_Web_View_TimeUI_ShowDate00cphTop_3734_pnlHijri'})
-        ghamari_date = ghamari_info.find('span', attrs={'class': 'show numeral'}).text
+    borjfalaki = soup.find('div', attrs={'class': 'astrologicalSign'}).\
+        find('span', attrs={'class': 'show sign'}).text
 
-        return ghamari_date
+    azan_sobh = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanMorning'}).text
 
-    def borjfalaki(self):
-        borjfalaki_info = self.soup.find('div', attrs={'class': 'astrologicalSign'})
-        borjfalaki_date = borjfalaki_info.find('span', attrs={'class': 'show sign'}).text
+    tolo_khorshid = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanSunrise'}).text
 
-        return borjfalaki_date
+    azan_zohr = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanNoon'}).text
 
-    def azan_sobh(self):
-        azan_sobh = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanMorning'}).text
+    ghrob_khorshid = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanSunset'}).text
 
-        return azan_sobh
+    azan_maghreb = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanNight'}).text
 
-    def tolo_khorshid(self):
-        tolo_khorshid = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanSunrise'}).text
-
-        return tolo_khorshid
-
-    def azan_zohr(self):
-        azan_zohr = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanNoon'}).text
-
-        return azan_zohr
-
-    def ghrob_khorshid(self):
-        ghorob_khorshid = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanSunset'}).text
-
-        return ghorob_khorshid
-
-    def azan_maghreb(self):
-        azan_maghreb = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblAzanNight'}).text
-
-        return azan_maghreb
-
-    def nime_shab(self):
-        nime_shab = self.soup.find('span', attrs={
-            'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblMidNight'}).text
-        return nime_shab
+    nime_shab = soup.find('span', attrs={
+        'id': 'ctl00_cphTop_Sampa_Web_View_EphemerisUI_EphemerisByCity12cphTop_3736_lblMidNight'}).text
